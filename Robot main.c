6f3b250cf9ad2ce33bfe67efd5d7de35326719a6
jobiniah <36646358+jobiniah@users.noreply.github.com>
2018-02-19 14:43:03 -0800

    Update and rename Robot.c to Robot main.c
    Added actual Base Code

---
+++ Robot main.c
@@ -0,0 +1,17 @@
+//	
+//	the main function for the robot
+//
+#include <xc.h> //for Robot from microchip.com?
+#include <stdbool.h> //because why not
+#include <math.h> //don't delay the inevitable
+#include "osc.h" //for chip freq
+#include "configuration_bits.h"
+
+void main(void){
+	set_osc_32MHz();
+	
+	while(1){
+	
+  }
+
+}
--- Robot.c
+++
@@ -1,3 +0,0 @@
-void main(void){
-printf("hello world");
-}
